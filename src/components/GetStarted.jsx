d44abc83245553db8f85799d9a46f90abbbf1956
Daniel Cruz <danielcunha54@gmail.com>
2023-02-13 08:42:49 -0300

    feat: create Hero component

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,5 +1,23 @@
-import React from 'react'
+import styles from '../styles.js'
+import { arrowUp } from '../assets'
 
 export default function GetStarted() {
-  return <div>GetStarted</div>
+  return (
+    <a
+      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+      href="#"
+    >
+      <div
+        className={`${styles.flexCenter} flex-col bg-primary w-full h-full rounded-full hover:opacity-95`}
+      >
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className="font-poppins font-medium text-[18px] leading-[23px] text-gradient mr-2">
+            Get
+          </p>
+          <img src={arrowUp} alt="Arrow" className="w-6 h-6 object-contain" />
+        </div>
+        <p className="font-poppins font-medium text-[18px] leading-[23px] text-gradient">Started</p>
+      </div>
+    </a>
+  )
 }
